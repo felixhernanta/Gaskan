104d9de8524508227371ec75cada7b199560db03
felixhernanta <felix.hernanta@ti.ukdw.ac.id>
2021-11-21 20:01:03 +0700

    Visual Mk4

--- src/main/java/id/ac/ukdw/fti/rpl/Gaskan/App.java
+++ src/main/java/id/ac/ukdw/fti/rpl/Gaskan/App.java
@@ -1,7 +1,5 @@
 package id.ac.ukdw.fti.rpl.Gaskan;
 
-import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -14,7 +12,6 @@ import javafx.stage.Stage;
  */
 public class App extends Application
 {
-    private static Scene scene;
 
     @Override
     public void start(Stage stage) throws Exception {
